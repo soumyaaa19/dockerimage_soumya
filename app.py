49c3f375d8eedd7e82748ad7855549df3124b505
soumyaaa19 <usoumya19@gmail.com>
2024-11-09 14:16:47 +0530

    Second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-    return "TIU is the best university in WB"
+    return "TIU is the best university in WB!"
 
 if __name__=='__main__':
     app.run(debug=True,host="0.0.0.0",port=5000)
--- test_app.py
+++ test_app.py
@@ -1,5 +1,4 @@
 from app import app
-
 def test_home():
     response=app.test_client().get("/")
 
